414bb7cbb718266f92ca85b7659d5f1063c81ddb
SK Manjur Ali <skmanjuralli9@gmail.com>
2022-12-20 12:48:32 +0530

    adsd

--- src/main/java/com/example/spring_jpa/Controller/MovieController.java
+++ src/main/java/com/example/spring_jpa/Controller/MovieController.java
@@ -14,7 +14,7 @@ public class MovieController {
     MovieService movieService;
 
     @GetMapping("/get_movies")
-    public List<Movie> getAllMovies() {
+    public List<Movie> getMovies() {
         return movieService.getAllMovies();
     }
 
