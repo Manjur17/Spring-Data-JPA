2181f7f2d31cacde1cdde92cd3d64cf78c96a472
SK Manjur Ali <skmanjuralli9@gmail.com>
2022-12-22 19:57:39 +0530

    add

--- src/main/java/com/example/spring_jpa/Respository/MovieRepository.java
+++ src/main/java/com/example/spring_jpa/Respository/MovieRepository.java
@@ -3,5 +3,5 @@ package com.example.spring_jpa.Respository;
 import com.example.spring_jpa.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Movie, String> { //<DatabaseName, Datatype of Primary key>
+public interface MovieRepository extends JpaRepository<Movie, String> { //<DatabaseName, Datatype of the Primary key>
 }
